60d3dc368062c4cfc6ab352f46464538b8b8e620
Khanmag <Khanmag329@gmail.com>
2023-01-25 09:39:32 +0300

    initialize

---
+++ script.js
@@ -0,0 +1,25 @@
+const uniqueAttributes = [
+    'accesskey',
+    'class',
+    'contenteditable',
+    'contextmenu',
+    'dir',
+    'hidden',
+    'id',
+    'lang',
+    'spellcheck',
+    'style',
+    'tabindex',
+    'title',
+    'xml:lang',
+]
+
+const tags = [
+    {
+        tag: 'a',
+        endTegRequired: true,
+        attributes: ['coords', 'download', 'href', 'hreflang', 'name', 'rel', 'rev',
+            'shape', 'target', 'title', 'type'],
+
+    }
+]
